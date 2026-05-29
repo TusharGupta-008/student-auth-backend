bdf50273471e1501cb1535e0393d6450a2646eb3
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-05-29 20:34:57 +0530

    making jwt verification

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -86,4 +86,95 @@
 
 // export { signUp, login };
 
+import bycryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        message: "Send all details. ",
+      });
+    }
+
+    const hashedPass = await bycryptjs.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPass,
+    });
+
+    res.status(201).json({
+      message: "User Created",
+      newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server Error",
+      error,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Details error",
+        error,
+      });
+    }
+    const existingEmail = await User.findOne({ email });
+
+    if (!existingEmail) {
+      return res.status(400).json({
+        message: "User does not exist",
+      });
+    }
+    const pass = await bycryptjs.compare(password, existingEmail.password);
+    if (!pass) {
+      return res.status(400).json({
+        message: "Password is wrong.",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingEmail.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "5d",
+      },
+    );
+
+    return res.status(201).json({
+      message: "Welcome back",
+      token,
+      user: {
+        id: existingEmail._id,
+        name: existingEmail.name,
+        email: existingEmail.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+      error,
+    });
+  }
+};
+
+const profile = async (req, res) => {
+  const user = await User.findById(req.user.id);
+  res.json({
+    message: "Profile fetched",
+    user,
+  });
+};
+
+export { signup, login, profile };
--- models/user.model.js
+++ models/user.model.js
@@ -21,3 +21,26 @@
 // const User = mongoose.model("user", userSchema);
 // export default User;
 
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("user", userSchema);
+export default User;
